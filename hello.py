47bbe87b6fdbbd47dd586c1c43b0fa25650f88e3
izaprz <izabelaprzybylo@gmail.com>
2016-12-10 13:20:11 +0100

    My Django Girls app, first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+def hej(imie):
+    print('Hej ' + imie + '!')
+
+
+dziewczyny = ["Iza", "Ania", "Sonia", "Patrycja", "Kasia"]
+for imie in dziewczyny:
+    hej(imie)
+    print("Witamy kolejną uczestniczkę")
+
+
+for i in range(1,10):
+    print(i)
